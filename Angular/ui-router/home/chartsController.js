1672c4e358d6031eb7d4d7d047b04082f9ee2206
Darpan <darpansheoran@gmail.com>
2021-06-25 10:51:41 +0530

    commit

--- Angular/ui-router/home/chartsController.js
+++ Angular/ui-router/home/chartsController.js
@@ -19,26 +19,55 @@ app.controller("chartsController", function ($scope, randomUser) {
 
   // update chart with data from api
   randomUser.getData().then(function (response) {
-    var males = 0;
-    var females = 0;
+    var ageGroup1 = 0;
+    var ageGroup2 = 0;
+    var ageGroup3 = 0;
+    var ageGroup4 = 0;
+    var ageGroup5 = 0;
+    var ageGroup6 = 0;
+
     for (i = 0; i < response.data.results.length; i++) {
-      if (response.data.results[i].gender == "male") {
-        males++;
+      if (response.data.results[i].dob.age <= 20) {
+        ageGroup1++;
+      } else if (
+        response.data.results[i].dob.age > 20 &&
+        response.data.results[i].dob.age <= 30
+      ) {
+        ageGroup2++;
+      } else if (
+        response.data.results[i].dob.age > 30 &&
+        response.data.results[i].dob.age <= 40
+      ) {
+        ageGroup3++;
+      } else if (
+        response.data.results[i].dob.age > 40 &&
+        response.data.results[i].dob.age <= 50
+      ) {
+        ageGroup4++;
+      } else if (
+        response.data.results[i].dob.age > 50 &&
+        response.data.results[i].dob.age <= 60
+      ) {
+        ageGroup5++;
       } else {
-        females++;
+        ageGroup6++;
       }
     }
 
     chartData = [
-      { label: "Males", value: males },
-      { label: "Females", value: females },
+      { label: "<20", value: ageGroup1 },
+      { label: "21-30", value: ageGroup2 },
+      { label: "31-40", value: ageGroup3 },
+      { label: "40-50", value: ageGroup4 },
+      { label: "50-60", value: ageGroup5 },
+      { label: ">60+", value: ageGroup6 },
     ];
 
     dataSource = {
       chart: {
-        caption: "Comparison of number of males and females",
+        caption: "Users based on their age",
         subCaption: "Data contains 100 results",
-        xAxisName: "Gender",
+        xAxisName: "Age group",
         yAxisName: "Number",
         numberSuffix: "",
         theme: "fusion",
